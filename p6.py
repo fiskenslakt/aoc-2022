49c3ad8782b59edd7dfb5959726480cea5c1e031
Derek <sjukledighet@gmail.com>
2022-12-06 00:39:22 -0500

    Day 6 solution

--- p6.py
+++ p6.py
@@ -1,9 +1,16 @@
-from aocd import data, submit
+from aocd import data
+
+start_of_packet = None
+start_of_message = None
 
 for i, char in enumerate(data):
-    # if len(set(data[i-4:i])) == 4:
-    #     break
+    if len(set(data[i-4:i])) == 4 and start_of_packet is None:
+        start_of_packet = i
     if len(set(data[i-14:i])) == 14:
+        start_of_message = i
+
+    if start_of_packet and start_of_message:
         break
 
-submit(i)
+print('Part 1:', start_of_packet)
+print('Part 2:', start_of_message)
