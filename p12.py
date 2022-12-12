d52b0e1c8827c4d6f54a6dc2b7e2c5aae70f2746
Derek <sjukledighet@gmail.com>
2022-12-12 02:51:36 -0500

    Day 12 solution

--- p12.py
+++ p12.py
@@ -1,50 +1,47 @@
 from collections import deque
 
-from aocd import lines, submit
+from aocd import lines
 
-# lines = '''Sabqponm
-# abcryxxl
-# accszExk
-# acctuvwj
-# abdefghi'''.splitlines()
 
-graph = {}
-queue = deque()
+def bfs(queue):
+    visited = set()
+    while queue:
+        (x, y), steps = queue.popleft()
+
+        for i, j in ((0,1), (1,0), (0,-1), (-1,0)):
+            nx = x+i
+            ny = y+j
+
+            if (nx < 0 or nx >= len(lines[0])
+                    or ny < 0 or ny >= len(lines)):
+                continue
+
+            cur_cell = lines[y][x]
+            new_cell = lines[ny][nx]
+            if (nx, ny) in visited:
+                continue
+
+            if new_cell == 'E':
+                if cur_cell == 'z':
+                    return steps + 1
+                continue
+
+            if (cur_cell == new_cell
+                    or ord(cur_cell) + 1 == ord(new_cell)
+                    or new_cell == 'a'
+                    or ord(cur_cell) > ord(new_cell)):
+                queue.append(((nx, ny), steps + 1))
+                visited.add((nx, ny))
+
+
+a_queue = deque()
+s_queue = deque()
 for y, row in enumerate(lines):
     for x, col in enumerate(row):
-        graph[(x,y)] = col
         if col == 'S':
-            start = (x, y)
+            s_queue.append(((x, y), 0))
         elif col == 'a':
-            queue.append(((x, y), 0))
+            a_queue.append(((x, y), 0))
 
-# import pudb;pu.db
-visited = {start}
-# queue = deque([(start, 0, [start])])
-while queue:
-    # (x, y), steps, path = queue.popleft()
-    (x, y), steps = queue.popleft()
-
-    if lines[y][x] == 'E':
-        print(steps, path)
-        break
-
-    for i, j in ((0,1), (1,0), (0,-1), (-1,0)):
-        nx = x+i
-        ny = y+j
-
-        if (nx < 0 or nx >= len(lines[0])
-                or ny < 0 or ny >= len(lines)):
-            continue
-
-        cur_cell = lines[y][x]
-        new_cell = lines[ny][nx]
-        if (cur_cell == new_cell or ord(cur_cell) + 1 == ord(new_cell) or new_cell in 'ab' or (ord(cur_cell) > ord(new_cell) and new_cell.islower())) and (nx,ny) not in visited:
-            # queue.append(((nx,ny), steps+1, path+[(nx,ny)]))
-            queue.append(((nx,ny), steps+1))
-            visited.add((nx,ny))
-        elif cur_cell == 'z' and new_cell == 'E':
-            submit(steps+1)
-            break
-else:
-    print('not found')
+print('Part 1:', bfs(s_queue))
+print('Part 2:', bfs(a_queue))
