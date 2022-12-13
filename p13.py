950af247dd099a06b369ebb14bad7370899e5718
Derek <sjukledighet@gmail.com>
2022-12-13 02:52:41 -0500

    Day 13 solution

--- p13.py
+++ p13.py
@@ -2,7 +2,9 @@ from ast import literal_eval
 from functools import cmp_to_key
 from itertools import zip_longest
 
-from aocd import data, submit
+from aocd import data
+
+DIVIDER_PACKETS = [[[2]], [[6]]]
 
 
 def check(left, right):
@@ -26,39 +28,29 @@ def check(left, right):
                 continue
             else:
                 return result
+        return 0
     elif isinstance(left, list) and isinstance(right, int):
         return check(left, [right])
     elif isinstance(left, int) and isinstance(right, list):
         return check([left], right)
 
-    return 0
-
 
 pairs = data.split('\n\n')
 indices = []
-
 for i, pair in enumerate(pairs, 1):
-    p1, p2 = pair.split()
-    p1 = literal_eval(p1)
-    p2 = literal_eval(p2)
-
+    p1, p2 = map(literal_eval, pair.split())
     result = check(p1, p2)
 
-    if result == 0:
-        continue
-    elif result == -1:
-        pass
-    elif result == 1:
+    if result == 1:
         indices.append(i)
 
 print('Part 1:', sum(indices))
 
-divider_packets = [[[2]], [[6]]]
-packets = list(map(literal_eval, data.split())) + divider_packets
+packets = list(map(literal_eval, data.split())) + DIVIDER_PACKETS
 packets = sorted(packets, key=cmp_to_key(check), reverse=True)
 
 indices = []
-for divider_packet in divider_packets:
+for divider_packet in DIVIDER_PACKETS:
     indices.append(packets.index(divider_packet) + 1)
 
 print('Part 2:', indices[0] * indices[1])
