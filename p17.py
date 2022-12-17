cd0a5bf1c4a15ff2560178015b3b7757e15ad56a
Derek <sjukledighet@gmail.com>
2022-12-17 13:20:03 -0500

    Day 17 raw solution

--- p17.py
+++ p17.py
@@ -34,13 +34,17 @@ highest_point = 0
 resting_rocks = 0
 chamber = {(0,0), (1,0), (2,0), (3,0), (4,0), (5,0), (6,0)}
 jet_idx = 0
+prev_states = {}
+part1, part2 = False, False
 
 # import pudb;pu.db
 
 for rock in cycle(ROCKS):
     if resting_rocks == 2022:
-        break
-    if resting_rocks == 1_000_000_000_000:
+        part1 = True
+        print('Part 1:', highest_point)
+
+    if part1 and part2:
         break
 
     x, y = 2, highest_point + 4
@@ -66,10 +70,31 @@ for rock in cycle(ROCKS):
                 resting_rocks += 1
                 for rx, ry in rock:
                     chamber.add((rx+x,ry+y))
+
+                fingerprint = ''
+                for j in range(highest_point, highest_point-20, -1):
+                    for i in range(7):
+                        if (i,j) in chamber:
+                            fingerprint += '#'
+                        else:
+                            fingerprint += ' '
+
+                state = (rock, jet_idx, fingerprint)
+                if state in prev_states and not part2:
+                    # print('found cycle:', resting_rocks, state)
+                    part2 = True
+                    cycle_length = resting_rocks - prev_states[state][1]
+                    height_delta = highest_point - prev_states[state][0]
+                else:
+                    prev_states[state] = (highest_point, resting_rocks)
                 break
         else:
             y = ny
             continue
         break
 
-submit(highest_point)
+rock_delta = int(1e12) % cycle_length
+height_offset = list(prev_states.values())[rock_delta-1][0]
+resting_rocks_extreme_edition = int(1e12 // cycle_length * height_delta) + height_offset
+print('Part 2:', resting_rocks_extreme_edition)
+submit(resting_rocks_extreme_edition)
